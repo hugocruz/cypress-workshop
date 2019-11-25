ff1802d1fe1671b77e0072607f6cb6950c9cc306
Hugo Cruz <hugo.cruz@finn.no>
2019-11-25 14:32:16 +0100

    fixture 1st approach

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,12 +1,9 @@
 describe('App initialization', () => {
     it('displays todos from api on load', () => {
         cy.server();
-        cy.route('GET', '/api/todos', [
-            { id: 1, name: 'Buy Milk', isComplete: false },
-            { id: 2, name: 'Buy Bread', isComplete: true },
-            { id: 3, name: 'Go home', isComplete: false },
-            { id: 4, name: 'relax', isComplete: false },
-        ]);
+        cy.fixture('todos').then((todos) => {
+            cy.route('GET', '/api/todos', todos);
+        })
         cy.visit('/');
         cy.get('.todo-list li').should('have.length', 4);
     });
