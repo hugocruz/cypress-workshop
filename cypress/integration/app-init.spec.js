6006dba805831e66596c2d2c73ba31840b3e5a88
Hugo Cruz <hugo.cruz@finn.no>
2019-11-25 14:34:14 +0100

    cleaning up fixture syntax

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,9 +1,7 @@
 describe('App initialization', () => {
     it('displays todos from api on load', () => {
         cy.server();
-        cy.fixture('todos').then((todos) => {
-            cy.route('GET', '/api/todos', todos);
-        })
+        cy.route('GET', '/api/todos', 'fixture:todos');
         cy.visit('/');
         cy.get('.todo-list li').should('have.length', 4);
     });
