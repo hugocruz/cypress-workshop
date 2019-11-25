4a501070ad7d6a88f9270ed2963d2191d43d74c2
Hugo Cruz <hugo.cruz@finn.no>
2019-11-25 14:39:17 +0100

    WAit for data to be loaded first

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,8 +1,9 @@
 describe('App initialization', () => {
     it('displays todos from api on load', () => {
         cy.server();
-        cy.route('GET', '/api/todos', 'fixture:todos');
+        cy.route('GET', '/api/todos', 'fixture:todos').as('load');
         cy.visit('/');
+        cy.wait('@load');
         cy.get('.todo-list li').should('have.length', 4);
     });
 });
