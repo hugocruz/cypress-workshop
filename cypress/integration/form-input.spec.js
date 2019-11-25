027dca74d1ab8c21452fc27cecb1457958f17c6a
Hugo Cruz <hugo.cruz@finn.no>
2019-11-25 15:46:16 +0100

    Changing customizign commands

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -4,7 +4,7 @@
 
 describe('Form input', () => {
     beforeEach(() => {
-        cy.loadAndVisit();
+        cy.loadAndVisit([]);
     })
     it('focus the input on load', () => {
         cy.focused().should('have.class', 'new-todo');
